be6adbdb41f8d4e19198b827483820b1e5d10090
Filip <filip26@gmail.com>
2025-09-11 21:04:55 +0200

    Add JakartaJsonTreeAdapter skeleton

---
+++ jakarta-json-api-adapter/src/main/java/com/apicatalog/tree/io/JakartaJsonTreeAdapter.java
@@ -0,0 +1,81 @@
+package com.apicatalog.tree.io;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.util.Collection;
+
+public class JakartaJsonTreeAdapter implements TreeAdapter {
+
+    @Override
+    public NodeType getNodeType(Object node) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Collection<Object> properties(Object node) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Object node(Object property, Object node) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Collection<Object> items(Object node) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String asString(Object node) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public boolean isDecimal(Object node) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public Integer asInteger(Object node) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Long asLong(Object node) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public BigInteger asBigInteger(Object node) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Double asDouble(Object node) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public BigDecimal asBigDecimal(Object node) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public byte[] asByteArray(Object node) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
