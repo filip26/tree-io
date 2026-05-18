41e963f80cf6042b50159126e93b1d99fefabd32
Steinar Bang <sb@dod.no>
2026-05-18 19:40:09 +0200

    Remove code for bumping tree-io-api dependencies in modules that have no effect
    The version of tree-io-api is set in <dependencyManagement> in the
    parent and the version in <dependencyManagement> is updated when the
    pom version is set with versions:set.

--- bin/bump-version.sh
+++ bin/bump-version.sh
@@ -15,15 +15,4 @@ echo "Bumping version to $NEW_VERSION..."
 echo "Updating main pom.xml..."
 mvn versions:set -DnewVersion="$NEW_VERSION" -DgenerateBackupPoms=false
 
-# 2. Update tree-io-api dependency in adapters
-for MODULE in "jakarta-adapter" "cbor-adapter" "jackson2-adapter"; do
-  echo "Updating tree-io-api dependency in $MODULE/pom.xml..."
-  mvn versions:use-dep-version \
-    -Dincludes=com.apicatalog:tree-io-api \
-    -DdepVersion="$NEW_VERSION" \
-    -DforceVersion=true \
-    -DgenerateBackupPoms=false \
-    -f "$MODULE/pom.xml"
-done
-
 echo "Version bump complete!"
